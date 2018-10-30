310f3d704d5d8ca9fc5c34e5cd45860d1d100baf
ahmedryed <36939373+ahmedryed@users.noreply.github.com>
2018-10-30 14:28:32 -0500

    Add files via upload

--- UtPod.cpp
+++ UtPod.cpp
@@ -2,3 +2,15 @@
 // Created by Ryed Ahmed on 10/30/2018.
 //
 
+#include <cstdlib>
+#include <iostream>
+#include "song.h"
+#include "UtPod.h"
+
+UtPod:: UtPod(int size){
+    if(size > MAX_MEMORY || size <= 0){
+        memSize = MAX_MEMORY;
+    }
+    else
+        memSize = size;
+}
