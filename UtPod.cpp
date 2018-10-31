41d785f8bbff78c4f2aea451180ef96246489d75
ahmedryed <36939373+ahmedryed@users.noreply.github.com>
2018-10-30 19:38:39 -0500

    Add files via upload

--- UtPod.cpp
+++ UtPod.cpp
@@ -7,10 +7,56 @@
 #include "song.h"
 #include "UtPod.h"
 
-UtPod:: UtPod(int size){
+
+UtPod::UtPod() {
+    songs = NULL;
+    memSize = MAX_MEMORY;
+}
+
+UtPod::UtPod(int size){
+    songs = NULL;
+
     if(size > MAX_MEMORY || size <= 0){
         memSize = MAX_MEMORY;
     }
     else
         memSize = size;
 }
+
+int UtPod::addSong(Song const &s){
+
+}
+
+int UtPod::removeSong(Song const &s){
+
+}
+
+void UtPod::shuffle(){
+
+}
+
+void UtPod::showSongList(){
+
+}
+
+void UtPod::sortSongList(){
+
+}
+
+void UtPod::clearMemory(){
+
+}
+
+int UtPod::getRemainingMemory(){
+    int usedMem = 0;
+    SongNode *ptr;
+
+    for(ptr = songs; ptr != NULL; ptr = ptr ->next){
+        usedMem += ptr->s.getSize();
+    }
+
+    int remainingMem = getTotalMemory() - usedMem;
+    return remainingMem;
+}
+
+UtPod::~UtPod(){}
